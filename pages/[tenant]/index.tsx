78a2e705468fffc66c476753e65a87aeb0fd32c2
icaroperetti <icaroperetti50@gmail.com>
2022-08-07 19:58:42 -0300

    Creating home

---
+++ pages/[tenant]/index.tsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/Home.module.css";
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}></div>
+        <div className={styles.headerBottom}>Buscar</div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
