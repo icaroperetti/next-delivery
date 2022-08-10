76bf3ead3ce3ee498de87a477ebeb90f7df5f375
icaroperetti <icaroperetti50@gmail.com>
2022-08-10 11:02:43 -0300

    Starting to style headerTop

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,10 +1,23 @@
 import styles from "../../styles/Home.module.css";
+
 const Home = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <div className={styles.headerTop}></div>
-        <div className={styles.headerBottom}>Buscar</div>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem Vindo (a) </div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje?</div>
+          </div>
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButton}>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerBottom}>...</div>
       </header>
     </div>
   );
